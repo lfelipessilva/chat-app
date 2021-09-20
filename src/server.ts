6986c43dad718b899af84a8ef0e544bc1b2497e2
luisfelipe <luis.felipeszsv@gmail.com>
2021-09-20 20:03:26 -0300

    starting over

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import { http } from './http';
-import './websocket/client';
-import './websocket/admin';
-
-const port = 3001;
-
-http.listen(port, () => {
-    console.log(`Server listening on ${port}`);
-});
